9bfb1833cc726dbbe61e57c4aca715d54ba84fe5
gambilldw <gambilldw@gmail.com>
2018-03-10 00:30:30 -0500

    Add files via upload

---
+++ Homework 12/db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Nothing Burger', FALSE);
